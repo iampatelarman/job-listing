a3ae845eaeeed7101f4a05753e428b0d5086a99c
Arman <arman.s.patel@gmail.com>
2023-12-09 16:20:14 +0530

    added database models

--- server/index.js
+++ server/index.js
@@ -8,10 +8,29 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const User = mongoose.model("User", {
+  name: String,
+  email: String,
+  phone: Number,
+  password: String,
+});
+
+const Job = mongoose.model("Job", {
+  title: String,
+  description: String,
+  minExperience: Number,
+  datePosted: Date,
+  reqSkills: String,
+  lastDate: Date,
+});
+
 app.get("/", (req, res) => {
   res.json({ status: "SUCCESS", message: "All good" });
 });
 
+app.get("/health", (req, res) => {
+  res.json({ server: "Job Listing", status: "active" });
+});
 app.listen(process.env.PORT, () => {
   mongoose
     .connect(process.env.MONGODB_URL)
